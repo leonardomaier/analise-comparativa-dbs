7122a7ee7d0214cd8d61046d4657a1f3f640ab06
Leonardo Maier <leonarmaier@gmail.com>
2025-04-01 14:14:40 -0300

    add sql script for creating postgres logs table

---
+++ create-pg-table.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS "analise-comparativa-dbs" CASCADE;
+
+CREATE SCHEMA "analise-comparativa-dbs";
+
+DROP TABLE IF EXISTS "analise-comparativa-dbs".logs;
+
+CREATE TABLE "analise-comparativa-dbs".logs (
+    id BIGSERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    log_level VARCHAR(50) NOT NULL,
+    message TEXT NOT NULL,
+    source VARCHAR(255) NOT NULL,
+    test_id BIGINT NOT NULL
+);
